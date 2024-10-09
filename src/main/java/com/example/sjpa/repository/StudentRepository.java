3accff33a65424f920849bec94b57010e1e26cdf
vadde sai kiran <vaddesaikiran8@gmail.com>
2024-10-09 13:26:30 +0530

    updates added

--- src/main/java/com/example/sjpa/repository/StudentRepository.java
+++ src/main/java/com/example/sjpa/repository/StudentRepository.java
@@ -3,7 +3,9 @@ package com.example.sjpa.repository;
 import com.example.sjpa.entity.Student;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+
 public interface StudentRepository extends PagingAndSortingRepository<Student, Long>{
+	
 	
 }
 
