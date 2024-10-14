2433de42e5d744569832e6019f7921125c6cf6c3
vadde sai kiran <vaddesaikiran8@gmail.com>
2024-10-14 12:34:10 +0530

    refresh option added

--- src/main/java/com/example/sjpa/entity/Book.java
+++ src/main/java/com/example/sjpa/entity/Book.java
@@ -11,7 +11,7 @@ public class Book {
     @Column(name = "title")
     private String title;
 
-    @OneToOne(cascade = {CascadeType.PERSIST,CascadeType.REMOVE,CascadeType.DETACH})
+    @OneToOne(cascade = {CascadeType.PERSIST,CascadeType.REMOVE,CascadeType.DETACH,CascadeType.REFRESH})
     @JoinColumn(name = "book_cover_page_id")
     private BookCoverPage bookCoverPage;
 
