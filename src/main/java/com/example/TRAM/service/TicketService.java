f8d922a3e49627f3f906c7dc3fb39b0fc9796136
vadde sai kiran <vaddesaikiran8@gmail.com>
2024-10-16 14:53:08 +0530

    transactional added

--- src/main/java/com/example/TRAM/service/TicketService.java
+++ src/main/java/com/example/TRAM/service/TicketService.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.TRAM.entity.BankAccount;
 import com.example.TRAM.entity.Payments;
@@ -27,6 +28,7 @@ public class TicketService {
     @Autowired
     private BankAccountRepository bankAccountRepository;
 
+    @Transactional
     public void bookTicket(UserInfo userInfo, Tickets ticket, Payments payment) {
         // Save ticket first
         logger.info("Saving ticket information...");
