306bd9ec32f482bee8ea877268642578b53f4f61
vadde sai kiran <vaddesaikiran8@gmail.com>
2024-10-22 14:56:28 +0530

    uodates added of sample aop

--- src/main/java/com/example/employeemanagement/aop/LoggingAspect.java
+++ src/main/java/com/example/employeemanagement/aop/LoggingAspect.java
@@ -1,24 +1,22 @@
 package com.example.employeemanagement.aop;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
+
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+
+@Component
 @Aspect
 public class LoggingAspect {
  private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
- @Before("execution(* com.example.employeemanagement.service.EmployeeService.getAllEmployees(..))")
+ @Before("execution(* *.*(..))")
  public void logBefore(JoinPoint joinPoint) {
-     logger.info("Entering method: {}", joinPoint.getSignature().getName());
- }
-
- @After("execution(* com.example.employeemanagement.service.EmployeeService.getAllEmployees(..))")
- public void logAfter(JoinPoint joinPoint) {
-     logger.info("Exiting method: {}", joinPoint.getSignature().getName());
+     logger.info("Entering method");
  }
 }
 
