2ce83bac87d54fff27ed5c846a4fac2e06b89612
vadde sai kiran <vaddesaikiran8@gmail.com>
2024-10-22 15:04:26 +0530

    updates

--- src/main/java/com/example/employeemanagement/aop/LoggingAspect.java
+++ src/main/java/com/example/employeemanagement/aop/LoggingAspect.java
@@ -6,7 +6,9 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+@Component
 @Aspect
 public class LoggingAspect {
  private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
